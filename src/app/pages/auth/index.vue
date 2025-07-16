99bcbf7371748bc82b42f2791057b3963313c514
18036291231@163.com <18036291231@163.com>
2025-07-16 09:05:42 +0800

    提交

--- src/app/pages/auth/index.vue
+++ src/app/pages/auth/index.vue
@@ -11,46 +11,47 @@
   </template>
   
   <script setup lang="ts">
-  import { ref } from 'vue'
-  import { redirect } from '@/utils/common'
-  import useMemberStore from '@/stores/member'
-  import { useLogin } from '@/hooks/useLogin'
-  import { toutiaoLoginByCode } from '@/app/api/auth'
-
+import { ref } from 'vue'
+import { redirect } from '@/utils/common'
+import useMemberStore from '@/stores/member'
+import { useLogin } from '@/hooks/useLogin'
+import { toutiaoLoginByCode } from '@/app/api/auth'
 
   const memberStore = useMemberStore()
   const login = useLogin()
   // 抖音登录
   const handleToutiaoLogin = () => {
-    if (typeof tt === 'undefined') {
-      uni.showToast({ title: '非抖音小程序环境', icon: 'none' })
-      return
+  if (typeof tt === 'undefined') {
+    uni.showToast({ title: '非抖音小程序环境', icon: 'none' })
+    return
+  }
+  tt.login({
+    success(res: any) {
+      if (res.code) {
+        uni.showLoading({ title: '' })
+        toutiaoLoginByCode({ code: res.code }).then((ret: any) => {
+          uni.hideLoading()
+          if (ret.data && ret.data.token) {
+            memberStore.setToken(ret.data.token)
+            login.handleLoginBack()
+          } else if (ret.data && ret.data.openid) {
+            uni.setStorageSync('openid', ret.data.openid)
+            if (ret.data.unionid) uni.setStorageSync('unionid', ret.data.unionid)
+            redirect({ url: '/app/pages/auth/bind', mode: 'redirectTo' })
+          } else {
+            uni.showToast({ title: ret.msg || '登录失败', icon: 'none' })
+          }
+        }).catch((err) => {
+          uni.hideLoading()
+          uni.showToast({ title: err.msg || '登录失败', icon: 'none' })
+        })
+      } else {
+        uni.showToast({ title: res.errMsg || '登录失败', icon: 'none' })
+      } },
+    fail() {
+      uni.showToast({ title: '登录失败', icon: 'none' })
     }
-    tt.login({
-        success(res: any) {
-        if (res.code) {
-          uni.showLoading({ title: '' })
-          toutiaoLoginByCode({ code: res.code }).then((ret: any) => {
-            uni.hideLoading()
-            if (ret.data && ret.data.token) {
-              memberStore.setToken(ret.data.token)
-              login.handleLoginBack()
-            } else {
-              uni.showToast({ title: ret.msg || '登录失败', icon: 'none' })
-            }
-          }).catch((err) => {
-            uni.hideLoading()
-            uni.showToast({ title: err.msg || '登录失败', icon: 'none' })
-          })
-        } else {
-          uni.showToast({ title: res.errMsg || '登录失败', icon: 'none' })
-        }
-      },
-      fail(err: any) {
-        uni.showToast({ title: '登录失败', icon: 'none' })
-      }
-
-    })
+  })
 }
   
   // 普通账号密码登录跳转
